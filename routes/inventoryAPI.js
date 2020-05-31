8542c2ff4cc945de747c3cf29cad36dbcd5e4efd
Zac Eskridge <zace118@gmail.com>
2020-05-30 21:21:44 -0700

    Route written for delete functionality in inventory. Plugged it into some code Evan wrote and it'sworking on his machine. About to pull from master after he pushes.

--- routes/inventoryAPI.js
+++ routes/inventoryAPI.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     // POST route for saving a new inventory item. We can create todo with the data in req.body
     app.post("/api/inventory/create", function (req, res) {
         console.log(req.body)
-        db.Inventory.create(req.body).then( (response) => {
+        db.Inventory.create(req.body).then((response) => {
             res.json(response);
 
         });
@@ -16,12 +16,13 @@ module.exports = function (app) {
         db.Inventory.findAll({
             where: {
                 TruckId: req.params.truckID
-              }
+            }
         }).then(function (crusine_db) {
             // returns a JSON object with table contents?????
 
             res.json(crusine_db);
         });
     });
+
 
 };
